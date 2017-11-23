ce2dd995293c1817b6dd3a48b150beb54eac1e0a
Marcos Rosa <marcos.cantor@gmail.com>
2017-11-23 17:59:54 -0200

    :+1:  using multiapplication

--- esb.py
+++ esb.py
@@ -1,14 +1,15 @@
 from app.connectors.db import DatabaseConnector
-from app import Instance
+from app.models.instance import Instance
 
 inst1 = Instance("app1", '127.0.0.1', 8080)
 app1 = inst1.initialize()
 con1 = DatabaseConnector("sqlite:///" + inst1.name + ".db")
 db1 = con1.connect(app1)
-# inst1.start(app1)
+inst1.start(app1)
 
 inst2 = Instance("app2", '127.0.0.1', 8081)
 app2 = inst2.initialize()
 con2 = DatabaseConnector("sqlite:///" + inst2.name + ".db")
 db2 = con2.connect(app2)
 inst2.start(app2)
+
