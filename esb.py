9ae507cebf5d2b0291ec2e7159371435b5911f89
Marcos Rosa <marcos.cantor@gmail.com>
2017-11-28 14:57:37 -0200

    initial configuration

--- esb.py
+++ esb.py
@@ -1,15 +1,11 @@
 from app.connectors.db import DatabaseConnector
 from app.models.instance import Instance
+from app import config_verify
 
-inst1 = Instance("app1", '127.0.0.1', 8080)
-app1 = inst1.initialize()
-con1 = DatabaseConnector("sqlite:///" + inst1.name + ".db")
-db1 = con1.connect(app1)
-inst1.start(app1)
+config = config_verify()
 
-inst2 = Instance("app2", '127.0.0.1', 8081)
-app2 = inst2.initialize()
-con2 = DatabaseConnector("sqlite:///" + inst2.name + ".db")
-db2 = con2.connect(app2)
-inst2.start(app2)
-
+instance = Instance(__name__, config['address'], config['port'])
+app = instance.initialize()
+connection = DatabaseConnector("sqlite:///" + instance.name + ".db")
+db = connection.connect(app)
+instance.start(app)
